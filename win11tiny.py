4d481129518b8779808652ceb973b9e5009d1b07
cuongtrinh69 <cuongtrinh6101@gmail.com>
2025-05-30 22:40:31 +0700

    Update win11tiny.py

--- win11tiny.py
+++ win11tiny.py
@@ -1,12 +1,14 @@
-import gdown
+import requests
 
-# URL file Google Drive
-url = "https://download937.mediafire.com/ialhefs3vbxg3xNJpqz_MMpMcQVZKxlXMEIgzJWsSNoovwAvFk8Ph8cTzciMDAB9-Ers5vaK0C2jX2rDFbct2UMeZi5vct4I7ydlFQCisDq3uiMU3rjZixlHc336i5A_erZ3dBveWMzSKztGFXqFvs70BCbuPmXve2W_x7PX-FaH/i1gjp9hqojf0z1g/tiny11.iso"
+url = "https://download937.mediafire.com/ialhefs3vbxg3wNJDqz_MPqMcQVZk1X19MEIgzJWsSNoowvAvFk8PBMcxkQ/win10.zip"
+output = "/mnt/win10.zip"
 
-# Đường dẫn lưu file sau khi tải
-output = "/mnt/win10.zip"  # Đổi tên file tùy ý
+print("Đang tải file...")
 
-# Tải file
-gdown.download(url, output, quiet=False)
+r = requests.get(url, stream=True)
+with open(output, "wb") as f:
+    for chunk in r.iter_content(chunk_size=8192):
+        if chunk:
+            f.write(chunk)
 
-print(f"File đã được tải về và lưu tại: {output}
+print(f"✅ File đã được tải về và lưu tại: {output}")
